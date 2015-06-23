8ccae5aa13af9c8b8836c3a5cec6948a7858c78e
Hector Corrada Bravo <hcorrada@gmail.com>
2015-06-23 17:50:16 -0500

    ggplot2 examples

--- Hector/dplyr.R
+++ Hector/dplyr.R
@@ -34,8 +34,51 @@ mean_delay <- mean(flights_fixed$delay, na.rm=TRUE)
 flights_fixed <- flights_fixed %>% 
   mutate(delay_fixed = ifelse(is_missing, mean_delay, delay))
  
- 
 
+# EDA
+
+# five number summary
+summary(flights$dep_delay)
+boxplot(flights$dep_delay, ylab="Dep. Delay")
+hist(flights$dep_delay)
+plot(density(flights$dep_delay, na.rm=TRUE))
+plot(density(flights$minute, na.rm=TRUE))
+
+# qq plot
+qqnorm(flights$dep_delay)
+
+# bivariate plot
+plot(flights$dep_delay, flights$arr_delay)
+
+# MA plot
+dep_m <- flights$dep_delay - flights$arr_delay
+dep_a <- (flights$dep_delay + flights$arr_delay) / 2
+plot(dep_a, dep_m)
+
+cor(flights$dep_delay, flights$arr_delay,
+    use="complete.obs")
+
+###
+boxplot(log(flights$dep_delay)~flights$carrier)
+mosaicplot(flights$origin~flights$carrier)
+
+hist(log2(flights$dep_delay))
+
+library(Lahman)
+library(ggplot2)
+batting <- tbl_df(Batting)
+batting %>% 
+  filter(yearID == "2010") %>%
+  ggplot(aes(x=AB, y=R, label=teamID, 
+             color=lgID, size=G)) +
+  geom_text() +
+  geom_point()
+
+batting %>%
+  filter(yearID == "2010") %>%
+  sample_n(100) %>%
+  ggplot(aes(x=AB, y=H,color=lgID)) +
+  geom_line()
 
 
 
